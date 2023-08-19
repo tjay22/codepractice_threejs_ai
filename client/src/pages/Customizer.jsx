e264eb4cdcd242dd39622f5770d95558bfd0fced
TJ Yquin <tahnee.yquin@gmail.com>
2023-08-19 10:44:02 -0700

    Modified Index & Customizer

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -62,13 +62,16 @@ const Customizer = () => {
 
     try {
       setGeneratingImg(true)
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ prompt }),
-      })
+      const response = await fetch(
+        "https://codepractice-threejs-ai.onrender.com/api/v1/dalle",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ prompt }),
+        }
+      )
 
       const data = await response.json()
 
